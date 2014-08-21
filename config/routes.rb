b4bbd13f06a77361a1f6a44e3e6fa88adbe5a9a7
Sergii Mertsalov <student@epicodus.com>
2014-08-21 14:28:49 -0700

    update recipes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   match('recipes/new', {:via => :get, :to => 'recipes#new'})
   match('recipes/:id', {:via => :get, :to => 'recipes#show'})
   match('recipes/:id/edit', {:via => :get, :to => 'recipes#edit'})
+  match('recipes/:id', {:via => [:patch, :put], :to => 'recipes#update'})
   match('/', {:via => :get, :to => 'recipes#index'})
 end
