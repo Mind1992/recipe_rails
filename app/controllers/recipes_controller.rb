9ec74ab34bc2d2103fbd4618f1e460b19dfaa3bd
Sergii Mertsalov <student@epicodus.com>
2014-08-21 11:34:11 -0700

    add route, controller, view for create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,24 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render('recipes/index.html.erb')
   end
+
+  def create
+    @recipe = Recipe.new(params[:recipe])
+    if @recipe.save
+      redirect_to("/recipes/#{@recipe.id}")
+    else
+      render("recipes/new.html.erb")
+    end
+  end
+
+  def new
+    @recipe = Recipe.new
+    render('recipes/new.html.erb')
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render('recipes/show.html.erb')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   match('recipes', {:via => :get, :to => 'recipes#index'})
+  match('recipes', {:via => :post, :to => 'recipes#create'})
+  match('recipes/new', {:via => :get, :to => 'recipes#new'})
+  match('recipes/show', {:via => :get, :to => 'recipes#show'})
 end
