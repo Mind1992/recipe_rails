9009b879df538976eb812c4884892a70d33de060
Sergii Mertsalov <student@epicodus.com>
2014-08-21 12:10:49 -0700

    add route for the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   match('recipes', {:via => :post, :to => 'recipes#create'})
   match('recipes/new', {:via => :get, :to => 'recipes#new'})
   match('recipes/:id', {:via => :get, :to => 'recipes#show'})
+  match('/', {:via => :get, :to => 'recipes#index'})
 end
